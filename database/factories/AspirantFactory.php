fd4444b9a2e3f9431b802077a87ca10c79a79fd6
Mahmud Bakale <apps-mahmud.bakale@gmail.com>
2026-04-18 15:55:58 +0100

    feat(AspirantFactory): Rename file and expand factory with additional attributes
    - Rename AspirantFactory file from extensionless to .php format
    - Add imports for Party, Office, Constituency models and Str utility
    - Generate slug from first name, last name, and unique number
    - Add party_id, office_id, and constituency_id relationships
    - Add state_id field initialization
    - Add bio field with faker paragraphs
    - Add photo_path field
    - Add social media fields (website, facebook, twitter, instagram)
    - Update is_incumbent to use weighted boolean (20% true)
    - Update is_active to use weighted boolean (90% true)
    - Improve email and phone generation with explicit method calls

---
+++ database/factories/AspirantFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Aspirant;
+use App\Models\Party;
+use App\Models\Office;
+use App\Models\Constituency;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Aspirant>
+ */
+class AspirantFactory extends Factory
+{
+    protected $model = Aspirant::class;
+
+    public function definition(): array
+    {
+        $firstName = $this->faker->firstName();
+        $lastName = $this->faker->lastName();
+
+        return [
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'slug' => Str::slug($firstName . ' ' . $lastName . '-' . $this->faker->unique()->numberBetween(1, 9999)),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'party_id' => Party::inRandomOrder()->first()?->id,
+            'office_id' => Office::inRandomOrder()->first()?->id,
+            'constituency_id' => Constituency::inRandomOrder()->first()?->id,
+            'state_id' => null,
+            'bio' => $this->faker->paragraphs(2, true),
+            'photo_path' => null,
+            'website' => $this->faker->optional()->url(),
+            'facebook' => $this->faker->optional()->userName(),
+            'twitter' => $this->faker->optional()->userName(),
+            'instagram' => $this->faker->optional()->userName(),
+            'is_incumbent' => $this->faker->boolean(20),
+            'is_active' => $this->faker->boolean(90),
+        ];
+    }
+}
